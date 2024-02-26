13703ae6f1535068f17a8eb5551eaea04653309e
Wingleszy <meowingless@mail.ru>
2024-02-26 17:10:03 +0300

    fetch change

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://v89188xu.beget.tech/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div className="registration__container">
-        <form action="" className='registration__form'>
-            <h2 className='password__form__name'>Авторизация</h2>
-            <input type="text" className='registration__input' placeholder='Логин'/>
-            <input type="password" className='registration__input' placeholder='Пароль'/>
-            <input type="submit" className='registration__submit' value='СОХРАНИТЬ'/>
-        </form>
+      <form action="" className='registration__form' onSubmit={handleSubmit}>
+        <h2 className='password__form__name'>Авторизация</h2>
+        <input
+          type="text"
+          className='registration__input'
+          placeholder='Логин'
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          type="password"
+          className='registration__input'
+          placeholder='Пароль'
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <input type="submit" className='registration__submit' value='СОХРАНИТЬ'/>
+      </form>
     </div>
-  )
-}
+  );
+};
